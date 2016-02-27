538ffa58ed169bfa2e7194f94f45171319381626
Mahdi Hussein <m@hdihusse.in>
2016-02-27 01:18:17 -0500

    Corrected whitelist for user model

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -2,10 +2,10 @@ class Users::RegistrationsController < Devise::RegistrationsController
   private
 
   def sign_up_params
-    params.require(:user).permit(:first_name, :last_name, :profession, :city, :state, :zip_code, :address, :email, :password, :password_confirmation, :current_password)
+    params.require(:user).permit(:first_name, :last_name, :profession, :dob, :nabp_id, :city, :state, :zip_code, :address, :email, :password, :password_confirmation, :current_password)
   end
 
   def account_update_params
-    params.require(:user).permit(:first_name, :last_name, :profession, :city, :state, :zip_code, :address, :email, :password, :password_confirmation, :current_password)
+    params.require(:user).permit(:first_name, :last_name, :profession, :dob, :nabp_id, :city, :state, :zip_code, :address, :email, :password, :password_confirmation, :current_password)
   end
 end
