a23dae726caf68ef89564ea08f1ab4ff95fc23d7
Cody Swann <cody@gunnertech.com>
2016-09-20 09:34:15 -0400

    minor

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,6 +65,7 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
 
   config.action_mailer.default_url_options = { :host => "courses.arealahec.org" }
+  config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
       :address   => "smtp.sendgrid.net",
       :port      => 587, # ports 587 and 2525 are also supported with STARTTLS
