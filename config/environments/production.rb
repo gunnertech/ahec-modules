50646a830f2c33ccc35f38f0f001078c565df2aa
Cody Swann <cody@gunnertech.com>
2016-09-20 08:56:54 -0400

    minor

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,6 +65,15 @@ Rails.application.configure do
   # config.action_mailer.raise_delivery_errors = false
 
   config.action_mailer.default_url_options = { :host => "courses.arealahec.org" }
+  config.action_mailer.smtp_settings = {
+      :address   => "smtp.sendgrid.net",
+      :port      => 587, # ports 587 and 2525 are also supported with STARTTLS
+      :enable_starttls_auto => true, # detects and uses STARTTLS
+      :user_name => ENV["SENDGRID_USERNAME"],
+      :password  => ENV["SENDGRID_PASSWORD"], # SMTP password is any valid API key, when user_name is "apikey".
+      :authentication => 'login',
+      :domain => 'courses.arealahec.org', # your domain to identify your server when connecting
+  }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
