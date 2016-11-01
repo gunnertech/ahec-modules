0cf65e6a18e76cc861691f6181174f0f2be73e6a
Cody Swann <cody@gunnertech.com>
2016-11-01 13:47:06 -0400

    minor

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  has_many :user_memberships
+  has_many :user_memberships, dependent: :destroy
   has_many :courses, :through => :user_memberships
 
   validates :email, uniqueness: true
