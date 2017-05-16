289053da75d7a70388c0ac7035fed16374506fbb
Cody Swann <cody@gunnertech.com>
2017-05-16 19:09:08 -0400

    minor

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -98,7 +98,8 @@ Rails.application.configure do
     :s3_credentials => {
       :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
       :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'],
-      :bucket => ENV['S3_BUCKET_NAME']
+      :bucket => ENV['S3_BUCKET_NAME'],
+      region: ENV['S3_REGION']
     }
   }
 end
