069ff0a5554f26452bb65401f648059f4cf9fbfd
Mahdi Hussein <m@hdihusse.in>
2017-06-15 15:02:30 -0400

    Fix Survey Admin -> User Auth, Close #35

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,6 +1,6 @@
 class CoursesController < ApplicationController
   before_filter :authorize_user
-  before_filter :authorize_admin, except: [:index, :time_spent_update, :register, :grade_quiz, :results, :display_certificate]
+  before_filter :authorize_admin, except: [:index, :time_spent_update, :register, :grade_quiz, :results, :display_certificate, :survey, :survey_response]
 
   before_action :set_course, only: [:show, :quiz, :grade_quiz, :register, :edit, :update, :destroy, :results, :display_certificate, :time_spent_update, :certificate_token, :survey, :survey_response]
   before_action :force_immutability_survey_response, only: [:survey, :survey_response]
