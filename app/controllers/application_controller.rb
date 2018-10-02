bd003d2e65be7b1d7fa51ad73b0d294c7a17c78d
Cody Swann <cody@gunnertech.com>
2018-10-02 12:26:31 -0400

    work

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -28,4 +28,16 @@ class ApplicationController < ActionController::Base
   def ssl_configured?
     !Rails.env.development?
   end
+
+  protect_from_forgery
+        
+  protected  
+    def after_sign_in_path_for(resource)
+      sign_in_url = new_user_session_url
+      if request.referer == sign_in_url
+        super
+      else
+        stored_location_for(resource) || request.referer || root_path
+      end
+    end
 end
