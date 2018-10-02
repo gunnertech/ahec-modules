6beab0a4dbdd6b597775c9e5f585489e2f6fd439
Cody Swann <cody@gunnertech.com>
2018-10-02 12:30:59 -0400

    work

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,35 +9,39 @@ class ApplicationController < ActionController::Base
     render layout: false
   end
 
-  def authorize_admin
-    redirect_to root_path, alert: 'Access Denied' unless (current_user && current_user.admin?)
-  end
-
-  def authorize_user
-    redirect_to root_path, alert: 'Access Denied' unless (current_user)
-  end
-
-  def after_sign_in_path_for(resource)
-    if current_user.admin
-      admin_join_requests_url
-    else
-      courses_path
-    end
-  end
-
-  def ssl_configured?
-    !Rails.env.development?
-  end
-
   protect_from_forgery
         
   protected  
+    def authorize_admin
+      redirect_to root_path, alert: 'Access Denied' unless (current_user && current_user.admin?)
+    end
+
+    def authorize_user
+      redirect_to new_user_session_url, alert: 'Access Denied' unless (current_user)
+    end
+
     def after_sign_in_path_for(resource)
-      sign_in_url = new_user_session_url
-      if request.referer == sign_in_url
-        super
+      if current_user.admin
+        admin_join_requests_url
       else
-        stored_location_for(resource) || request.referer || root_path
+        sign_in_url = new_user_session_url
+        if request.referer == sign_in_url
+          super
+        else
+          stored_location_for(resource) || request.referer || root_path
+        end
       end
     end
+
+    def ssl_configured?
+      !Rails.env.development?
+    end
+    # def after_sign_in_path_for(resource)
+    #   sign_in_url = new_user_session_url
+    #   if request.referer == sign_in_url
+    #     super
+    #   else
+    #     stored_location_for(resource) || request.referer || root_path
+    #   end
+    # end
 end
